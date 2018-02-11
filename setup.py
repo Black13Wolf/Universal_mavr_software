922644b04004d79ef6f73994023aed23771ab8ad
Beskakotov_work <beskakotov.as@gmail.com>
2018-02-11 16:26:48 +0300

    fix

--- setup.py
+++ setup.py
@@ -8,6 +8,5 @@ setup(name='linux_mavr_software',
       author_email='beskakotov.as@gmail.com',
       url='https://github.com/Black13Wolf/linux_mavr_module',
       packages=['mavrControl',],
-      install_requires=['mavr_module', 'PyQt5', 'numpy', 'scipy', 'matplotlib', 'pyqtgraph'],
-      dependency_links=['git+https://github.com/black13wolf/mavr_module'],
+      install_requires=['PyQt5', 'numpy', 'scipy', 'matplotlib', 'pyqtgraph'],
      )
