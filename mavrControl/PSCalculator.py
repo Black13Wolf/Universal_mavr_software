e0ab273ff75823cb39d047337efd3747c258c4c3
Beskakotov_work <beskakotov.as@gmail.com>
2018-02-12 17:29:22 +0300

    Настройка PSCalculator

--- mavrControl/PSCalculator.py
+++ mavrControl/PSCalculator.py
@@ -20,26 +20,59 @@ class PSCalculator(QWidget):
         self.l_Input = QLabel('Input path')
         self.v_Input = QLineEdit()
 
-        self.l_Type = QLabel('Type')
+        self.l_Type = QLabel('Type: ')
         self.v_Type = QComboBox()
         self.v_Type.addItems(['Year', 'Set', 'Night', 'Star'])
+        self.l_Type.setFixedWidth(150)
+        self.v_Type.setFixedWidth(150)
         
+        self.l_Diff = QLabel('Diff frames: ')
+        self.v_Diff = QSpinBox()
+        self.l_Diff.setFixedWidth(150)
+        self.v_Diff.setFixedWidth(150)
+        
+        self.l_Acf = QLabel('ACF')
+        self.v_Acf = QComboBox()
+        self.v_Acf.addItems(['ON', 'OFF'])
+        
+        self.l_Save = QLabel('Save: ')
+        self.v_Save = QComboBox()
+        self.v_Save.addItems(['ON', 'OFF'])
+
+        self.l_Shape = QLabel('Shape: ')
+        self.v_Shape = QComboBox()
+        self.v_Shape.addItems(['512x512', '1024x1024', '2048x2048', '4096x4096'])
+
+        self.l_Rmbgr = QLabel('Removing BGR: ')
+        self.v_Rmbgr = QComboBox()
+        self.v_Rmbgr.addItems(['ON', 'OFF'])
+
         self.l_Output = QLabel('Output path: ')
-        self.v_Outout = QLineEdit()
+        self.v_Output = QLineEdit()
+    #def get_ps(path_to_dat, diff=0, acf=False, save=False, shape=(512,512), output=False, rmbgr_on=True)
         
         self.layout.addWidget(self.l_Input, 0, 0)
         self.layout.addWidget(self.v_Input, 0, 1)
         self.layout.addWidget(self.b_Input, 0, 2, 1, 2)
         self.layout.addWidget(self.l_Output, 1, 0)
-        self.layout.addWidget(self.v_Outout, 1, 1)
+        self.layout.addWidget(self.v_Output, 1, 1)
         self.layout.addWidget(self.b_Output, 1, 2, 1, 2)
         self.layout.addWidget(self.l_Type, 2, 0)
         self.layout.addWidget(self.v_Type, 2, 1)
+        self.layout.addWidget(self.l_Diff, 2, 2)
+        self.layout.addWidget(self.v_Diff, 2, 3)
+        self.layout.addWidget(self.l_Acf, 3, 0)
+        self.layout.addWidget(self.v_Acf, 3, 1)
+        self.layout.addWidget(self.l_Save, 3, 2)
+        self.layout.addWidget(self.v_Save, 3, 2)
+        self.layout.addWidget(self.l_Shape, 4, 0)
+        self.layout.addWidget(self.v_Shape, 4, 1)
+        self.layout.addWidget(self.l_Rmbgr, 4, 2)
+        self.layout.addWidget(self.v_Rmbgr, 4, 3)
         self.layout.addWidget(self.b_Start, 10, 1, 1, 2)
         self.setLayout(self.layout)
     
 
-    #def get_ps(path_to_dat, diff=0, acf=False, save=False, shape=(512,512), output=False, rmbgr_on=True)
     def c_Start(self):
         self.th = Thread(target = get_ps, args=())
 
