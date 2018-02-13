2b1fa98b38a36033b062378cfa5c094c3629213d
Beskakotov_work <beskakotov.as@gmail.com>
2018-02-13 13:21:03 +0300

    alpha version PSCalculator

--- mavrControl/PSCalculator.py
+++ mavrControl/PSCalculator.py
@@ -3,7 +3,10 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 import sys
 from mavr.processing import get_ps
-from threading import Thread
+from threading import Thread, active_count
+import PS_base_scanner as base_scan
+from ast import literal_eval
+from time import sleep
 
 class PSCalculator(QWidget):
     def __init__(self, parent = None):
@@ -37,11 +40,11 @@ class PSCalculator(QWidget):
         
         self.l_Save = QLabel('Save: ')
         self.v_Save = QComboBox()
-        self.v_Save.addItems(['ON', 'OFF'])
+        self.v_Save.addItems(['fits', 'OFF'])
 
         self.l_Shape = QLabel('Shape: ')
         self.v_Shape = QComboBox()
-        self.v_Shape.addItems(['512x512', '1024x1024', '2048x2048', '4096x4096'])
+        self.v_Shape.addItems(['(1024,1024)', '(512,512)', '(2048,2048)', '(4096,4096)'])
 
         self.l_Rmbgr = QLabel('Removing BGR: ')
         self.v_Rmbgr = QComboBox()
@@ -49,8 +52,20 @@ class PSCalculator(QWidget):
 
         self.l_Output = QLabel('Output path: ')
         self.v_Output = QLineEdit()
-    #def get_ps(path_to_dat, diff=0, acf=False, save=False, shape=(512,512), output=False, rmbgr_on=True)
         
+        #___ Loadingbars
+
+        self.loading_bars = {
+            'sets' : QProgressBar(),
+            'nights' : QProgressBar(),
+            'stars' : QProgressBar()
+        }
+        self.loading_bars['sets'].setFormat('Sets')
+        self.loading_bars['sets'].hide()
+        self.loading_bars['nights'].setFormat('Nights')
+        self.loading_bars['nights'].hide()
+        self.loading_bars['stars'].setFormat('Stars')
+        self.loading_bars['stars'].hide()
         self.layout.addWidget(self.l_Input, 0, 0)
         self.layout.addWidget(self.v_Input, 0, 1)
         self.layout.addWidget(self.b_Input, 0, 2, 1, 2)
@@ -70,14 +85,75 @@ class PSCalculator(QWidget):
         self.layout.addWidget(self.l_Rmbgr, 4, 2)
         self.layout.addWidget(self.v_Rmbgr, 4, 3)
         self.layout.addWidget(self.b_Start, 10, 1, 1, 2)
+        self.layout.addWidget(self.loading_bars['sets'], 11, 0, 1, 4)
+        self.layout.addWidget(self.loading_bars['nights'], 12, 0, 1, 4)
+        self.layout.addWidget(self.loading_bars['stars'], 13, 0, 1, 4)
         self.setLayout(self.layout)
     
 
     def c_Start(self):
-        self.th = Thread(target = get_ps, args=())
-
+        self.b_Start.setEnabled(False)
+        [self.loading_bars[i].hide() for i in self.loading_bars]
+        params = {}
+        params['input'] = {self.v_Type.currentText().lower() : self.v_Input.text()}
+        params['output'] = {self.v_Type.currentText().lower() : self.v_Output.text()}
+        params['type'] = self.v_Type.currentText().lower()
+        params['diff'] = self.v_Diff.value() 
+        if self.v_Acf.currentText() == 'ON':
+            params['acf'] = True
+        elif self.v_Acf.currentText() == 'OFF':
+            params['acf'] = False
+        if self.v_Save.currentText() == 'OFF':
+            params['save'] = False
+        else: params['save'] = self.v_Save.currentText()
+        params['shape'] = literal_eval(self.v_Shape.currentText())
+        if self.v_Rmbgr.currentText() == 'ON':
+            params['rmbgr'] = True
+        elif self.v_Rmbgr.currentText() == 'OFF':
+            params['rmbgr'] = False
+        if params['type'] == 'year':
+            self.loading_bars['sets'].show()
+            self.loading_bars['nights'].show()
+            self.loading_bars['stars'].show()
+            self.th = {
+                '1' : Thread(target = base_scan.scan_year, args=(params, self.loading_bars)),
+                '2' : Thread(target = self.check_of_end, args=(self.b_Start,))
+            }
+        elif params['type'] == 'set':
+            self.loading_bars['nights'].show()
+            self.loading_bars['stars'].show()
+            self.th = {
+                '1' : Thread(target = base_scan.scan_set, args=(params, self.loading_bars)),
+                '2' : Thread(target = self.check_of_end, args=(self.b_Start,))
+            }
+        elif params['type'] == 'night':
+            self.loading_bars['stars'].show()
+            self.th = {
+                '1' : Thread(target = base_scan.scan_night, args=(params, self.loading_bars)),
+                '2' : Thread(target = self.check_of_end, args=(self.b_Start,))
+            }
+        elif params['type'] == 'star':
+            #def get_ps(path_to_dat, diff=0, acf=False, save=False, shape=(512,512), output=False, rmbgr_on=True):
+            self.th = {
+                '1' : Thread(target = get_ps, args = (params['input']['star'],), kwargs={'diff':params['diff'], 'acf':params['acf'], 'save':params['save'], 'shape':params['shape'], 'output':params['output']['star'], 'rmbgr_on':params['rmbgr']}),
+                '2' : Thread(target = self.check_of_end, args=(self.b_Start,))
+            }
+        else:
+            return 1
+        self.th['1'].start()
+        sleep(0.5)
+        self.th['2'].start()
     def c_Input(self):
-        pass
+        if self.v_Type.currentText().lower() == 'star':
+            self.v_Input.setText(QFileDialog.getOpenFileName(self, 'Select Input Dat File', '.', "DAT (*.dat)")[0])
+        else:
+            self.v_Input.setText(QFileDialog.getExistingDirectory(self, "Select Input Directory", '.', QFileDialog.ShowDirsOnly))
 
     def c_Output(self):
-        pass
+        self.v_Output.setText(QFileDialog.getExistingDirectory(self, "Select Output Directory", '.', QFileDialog.ShowDirsOnly))
+    
+    def check_of_end(self, button):
+        while True:
+            if active_count() == 2:
+                button.setEnabled(True)
+                break
---
+++ mavrControl/PS_base_scanner.py
@@ -0,0 +1,63 @@
+﻿from os.path import join, isdir, basename
+from os import walk, makedirs, system
+import sys
+from mavr.processing import get_ps
+from time import time
+from numpy import mean
+
+def scan_year(params, loading_bars=False):
+    sets = list(walk(params['input']['year']))[0][1]
+    if loading_bars:
+        loading_bars['sets'].setRange(0, len(sets))
+        num_set = 0
+    for month in sets:
+        params['input']['set'] = join(params['input']['year'], month)
+        params['output']['set'] = join(params['output']['year'], month)
+        scan_set(params, loading_bars)
+        if loading_bars:
+            num_set += 1
+            loading_bars['year'].setValue(num_set)
+
+def scan_set(params, loading_bars):
+    if isdir(join(params['input']['set'], 'cut')):
+        params['input']['subdir'] = join(params['input']['set'], 'cut')
+    elif isdir(join(params['input']['set'], 'rebuild')):
+        params['input']['subdir'] = join(params['input']['set'], 'rebuild')
+    elif isdir(join(params['input']['set'], 'rebuilded')):
+        params['input']['subdir'] = join(params['input']['set'], 'rebuilded')
+    else:
+        print('Error')
+        return 1
+    nights = list(walk(params['input']['subdir']))[0][1]
+    if loading_bars:
+        loading_bars['nights'].setRange(0, len(nights))
+        num_night = 0
+    for night in nights:
+        params['input']['night'] = join(params['input']['subdir'], night)
+        params['output']['night'] = join(params['output']['set'], night)
+        scan_night(params, loading_bars)
+        if loading_bars:
+            num_night += 1
+            loading_bars['nights'].setValue(num_night)
+
+def scan_night(params, loading_bars=False):
+    stars = []
+    try:
+        makedirs(params['output']['night'])
+    except:
+        pass
+    for root, dirs, files in walk(params['input']['night']):
+        for name in files:
+            if not name.endswith('.dat') and name.startswith('dark') and name.startswith('flat') and 'moon' in name:
+                continue
+            else:
+                stars.append(join(root, name))
+        break
+    if loading_bars:
+        loading_bars['stars'].setRange(0, len(stars))
+        num_star = 0
+    for star in stars:
+        get_ps(star, diff=params['diff'], acf=params['acf'], save=params['save'], shape=params['shape'], output=params['output']['night'], rmbgr_on=params['rmbgr'])
+        if loading_bars:
+            num_star += 1
+            loading_bars['stars'].setValue(num_star)
