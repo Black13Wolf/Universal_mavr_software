89b1643fb49d1f221eed7b46dad7143e97094e80
Beskakotov_work <beskakotov.as@gmail.com>
2018-02-19 12:09:12 +0300

    debug

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from mavrControl import __version__
 setup(name='linux_mavr_software',
       version=__version__,
@@ -6,6 +6,6 @@ setup(name='linux_mavr_software',
       author='Anatoly Beskakotov',
       author_email='beskakotov.as@gmail.com',
       url='https://github.com/Black13Wolf/linux_mavr_module',
-      packages=['mavrControl',],
+      packages=find_packages(),
       install_requires=['PyQt5>=5.10', 'numpy>=1.14', 'scipy>=1.0', 'matplotlib>=2.1.2', 'pyqtgraph>=0.10'],
      )
